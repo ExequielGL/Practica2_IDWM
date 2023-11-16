a9c1c300261682bef3cf4f27fabe08c1ad9b0e84
ExequielGL <exequiel.gonzales@gmail.com>
2023-11-16 04:19:11 -0300

    Configuracion ruta frontend

--- frontend_practica2/src/app/app-routing.module.ts
+++ frontend_practica2/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ProfileComponent } from './components/profile/profile.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
+  { path: 'portfolio', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
